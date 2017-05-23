2c7f213c60728577a76245ce2408952954b15419
gamcildowie <gamci1@student.monash.edu>
2017-05-23 14:26:12 +1000

    day

---
+++ reader/assets/js/main3.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+
+  $('.number').mouseenter(function() {
+   $('.footnote').addClass('showme');
+ });
+
+ $('.number').mouseleave(function() {
+   $('.footnote').removeClass('showme');
+ });
+
+
+
+});

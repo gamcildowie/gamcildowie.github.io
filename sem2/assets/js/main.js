a5d74f3986d065e7acaf7ae0f1058bafdc7c60a8
gamcildowie <gamci1@student.monash.edu>
2017-08-09 01:46:34 +1000

    wk3 posterclub

--- sem2/assets/js/main.js
+++ sem2/assets/js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
 
 
-// Add jQuery here
+  setTimeout(function () {
+       $('.redsquare' '.low9' 'low8' 'high').addClass('addwhite');
+     }, 1000);
+
+     setTimeout(function () {
+       $('.redsquare''.low').addClass('spin');
+     }, 3000);
+
 
 
 

a4432de844fd76728e19f691e452574d6e234345
gamcildowie <gamci1@student.monash.edu>
2017-08-15 16:53:03 +1000

    posterclub
    tuesday 15th - homepage and levels completed

--- sem2/assets/js/main.js
+++ sem2/assets/js/main.js
@@ -1,24 +1,28 @@
 $(document).ready(function() {
 
-  $('.header').hover(function() {
+
+
+  $('.navigation, .menu.showme').hover(function() {
     $('.menu').addClass('showme');
-  });
-  $('.header').click(function() {
-    $('.menu').removeClass('showme');
+    $('.menu').mouseleave(function() {
+      $('.menu').removeClass('showme');
+    });
   });
 
-  $('.navigation').hover(function() {
-    $('.header').addClass('showme');
+
+  $('.box4').hover(function() {
+    $('.box4,.content,.info,.apostraphy').addClass('showme');
   });
-  $('.navigation').click(function() {
-    $('.header').removeClass('showme');
+
+  $('.box4').click(function() {
+    $('.box4,.content,.info,.apostraphy').removeClass('showme');
   });
 
 
 
 
   setTimeout(function () {
-       $('.redsquare, .low9,.low8, .high').addClass('addwhite');
+       $('.low9,.low8').addClass('addwhite');
      }, 1000);
 
      setTimeout(function () {
@@ -28,12 +32,52 @@ $(document).ready(function() {
 
 
 
+     var audioCar = document.createElement('audio');
+            audioCar.setAttribute('src', 'https://www.soundjay.com/transportation/sounds/car-ignition-2.mp3');
+            $.get();
+            audioCar.addEventListener("load", function() {
+              audioCar.play();
+            }, true);
+
+
+            var audioKey = document.createElement('audio');
+                   audioKey.setAttribute('src', 'https://www.soundjay.com/transportation/sounds/key-ignition-insert-1.mp3');
+                   $.get();
+                   audioKey.addEventListener("load", function() {
+                     audioKey.play();
+                   }, true);
+
+                   var audioHonk = document.createElement('audio');
+                          audioHonk.setAttribute('src', 'https://www.soundjay.com/transportation/sounds/car-locked-honk-1.mp3');
+                          $.get();
+                          audioHonk.addEventListener("load", function() {
+                            audioHonk.play();
+                          }, true);
 
 
 
 
+       $('.blackbox').hover(function() {
+               audioKey.play();
+             });
 
 
+            $('.blackbox').click(function() {
+                    audioCar.play();
+                  });
+
+                  $('.blackbox').click(function() {
+                    $('.blackbox').toggleClass('showme');
+                    $('.showme').mouseleave(function() {
+                    $('.blackbox').removeClass('showme');
+                  });
+                        });
+
+
+
+                      $('.fiat').click(function() {
+                              audioHonk.play();
+                            });
 
 
 
